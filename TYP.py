9ee453753e3f76de08edc9fe633d4cd9d552bf73
Cope, Louis(Student) <l.cope@lancaster.ac.uk>
2026-01-28 22:34:18 +0000

    Rewriting of a lot of processing code, using zmq for data transfer from GNURadio to my laptop. Data is partly understandable but does not seem to identify presences yet, will be investigating.

--- TYP.py
+++ TYP.py
@@ -20,9 +20,8 @@ from PyQt5 import Qt
 from argparse import ArgumentParser
 from gnuradio.eng_arg import eng_float, intx
 from gnuradio import eng_notation
-from gnuradio import network
 from gnuradio import soapy
-import sip
+from gnuradio import zeromq
 import threading
 
 
@@ -71,159 +70,32 @@ class TYP(gr.top_block, Qt.QWidget):
         # Blocks
         ##################################################
 
-        self.soapy_bladerf_source_0 = None
+        self.zeromq_pub_sink_0 = zeromq.pub_sink(gr.sizeof_float, 1, 'tcp://127.0.0.1:5000', 100, False, (-1), '', True, True)
+        self.soapy_bladerf_source_1 = None
         dev = 'driver=bladerf'
         stream_args = ''
         tune_args = ['']
         settings = ['']
 
-        self.soapy_bladerf_source_0 = soapy.source(dev, "fc32", 1, '',
+        self.soapy_bladerf_source_1 = soapy.source(dev, "fc32", 1, '',
                                   stream_args, tune_args, settings)
-        self.soapy_bladerf_source_0.set_sample_rate(0, samp_rate)
-        self.soapy_bladerf_source_0.set_bandwidth(0, bandwidth)
-        self.soapy_bladerf_source_0.set_frequency(0, centre_freq)
-        self.soapy_bladerf_source_0.set_frequency_correction(0, 0)
-        self.soapy_bladerf_source_0.set_gain(0, min(max(10.0, -1.0), 60.0))
-        self.qtgui_number_sink_0 = qtgui.number_sink(
-            gr.sizeof_float,
-            0,
-            qtgui.NUM_GRAPH_HORIZ,
-            1,
-            None # parent
-        )
-        self.qtgui_number_sink_0.set_update_time(0.10)
-        self.qtgui_number_sink_0.set_title("")
-
-        labels = ['', '', '', '', '',
-            '', '', '', '', '']
-        units = ['', '', '', '', '',
-            '', '', '', '', '']
-        colors = [("black", "black"), ("black", "black"), ("black", "black"), ("black", "black"), ("black", "black"),
-            ("black", "black"), ("black", "black"), ("black", "black"), ("black", "black"), ("black", "black")]
-        factor = [1, 1, 1, 1, 1,
-            1, 1, 1, 1, 1]
-
-        for i in range(1):
-            self.qtgui_number_sink_0.set_min(i, -100)
-            self.qtgui_number_sink_0.set_max(i, 100)
-            self.qtgui_number_sink_0.set_color(i, colors[i][0], colors[i][1])
-            if len(labels[i]) == 0:
-                self.qtgui_number_sink_0.set_label(i, "Data {0}".format(i))
-            else:
-                self.qtgui_number_sink_0.set_label(i, labels[i])
-            self.qtgui_number_sink_0.set_unit(i, units[i])
-            self.qtgui_number_sink_0.set_factor(i, factor[i])
-
-        self.qtgui_number_sink_0.enable_autoscale(False)
-        self._qtgui_number_sink_0_win = sip.wrapinstance(self.qtgui_number_sink_0.qwidget(), Qt.QWidget)
-        self.top_layout.addWidget(self._qtgui_number_sink_0_win)
-        self.qtgui_histogram_sink_x_0 = qtgui.histogram_sink_f(
-            2048,
-            27,
-            (-100),
-            100,
-            "",
-            1,
-            None # parent
-        )
-
-        self.qtgui_histogram_sink_x_0.set_update_time(0.10)
-        self.qtgui_histogram_sink_x_0.enable_autoscale(True)
-        self.qtgui_histogram_sink_x_0.enable_accumulate(False)
-        self.qtgui_histogram_sink_x_0.enable_grid(False)
-        self.qtgui_histogram_sink_x_0.enable_axis_labels(True)
-
-
-        labels = ['', '', '', '', '',
-            '', '', '', '', '']
-        widths = [1, 1, 1, 1, 1,
-            1, 1, 1, 1, 1]
-        colors = ["blue", "red", "green", "black", "cyan",
-            "magenta", "yellow", "dark red", "dark green", "dark blue"]
-        styles = [1, 1, 1, 1, 1,
-            1, 1, 1, 1, 1]
-        markers= [-1, -1, -1, -1, -1,
-            -1, -1, -1, -1, -1]
-        alphas = [1.0, 1.0, 1.0, 1.0, 1.0,
-            1.0, 1.0, 1.0, 1.0, 1.0]
-
-        for i in range(1):
-            if len(labels[i]) == 0:
-                self.qtgui_histogram_sink_x_0.set_line_label(i, "Data {0}".format(i))
-            else:
-                self.qtgui_histogram_sink_x_0.set_line_label(i, labels[i])
-            self.qtgui_histogram_sink_x_0.set_line_width(i, widths[i])
-            self.qtgui_histogram_sink_x_0.set_line_color(i, colors[i])
-            self.qtgui_histogram_sink_x_0.set_line_style(i, styles[i])
-            self.qtgui_histogram_sink_x_0.set_line_marker(i, markers[i])
-            self.qtgui_histogram_sink_x_0.set_line_alpha(i, alphas[i])
-
-        self._qtgui_histogram_sink_x_0_win = sip.wrapinstance(self.qtgui_histogram_sink_x_0.qwidget(), Qt.QWidget)
-        self.top_layout.addWidget(self._qtgui_histogram_sink_x_0_win)
-        self.qtgui_freq_sink_x_0 = qtgui.freq_sink_f(
-            1024, #size
-            window.WIN_BLACKMAN_hARRIS, #wintype
-            centre_freq, #fc
-            bandwidth, #bw
-            "", #name
-            1,
-            None # parent
-        )
-        self.qtgui_freq_sink_x_0.set_update_time(0.10)
-        self.qtgui_freq_sink_x_0.set_y_axis((-140), 30)
-        self.qtgui_freq_sink_x_0.set_y_label('Relative Gain', 'dB')
-        self.qtgui_freq_sink_x_0.set_trigger_mode(qtgui.TRIG_MODE_FREE, 0.0, 0, "")
-        self.qtgui_freq_sink_x_0.enable_autoscale(False)
-        self.qtgui_freq_sink_x_0.enable_grid(True)
-        self.qtgui_freq_sink_x_0.set_fft_average(1.0)
-        self.qtgui_freq_sink_x_0.enable_axis_labels(True)
-        self.qtgui_freq_sink_x_0.enable_control_panel(False)
-        self.qtgui_freq_sink_x_0.set_fft_window_normalized(False)
-
-
-        self.qtgui_freq_sink_x_0.set_plot_pos_half(not True)
-
-        labels = ['', '', '', '', '',
-            '', '', '', '', '']
-        widths = [1, 1, 1, 1, 1,
-            1, 1, 1, 1, 1]
-        colors = ["blue", "red", "green", "black", "cyan",
-            "magenta", "yellow", "dark red", "dark green", "dark blue"]
-        alphas = [1.0, 1.0, 1.0, 1.0, 1.0,
-            1.0, 1.0, 1.0, 1.0, 1.0]
-
-        for i in range(1):
-            if len(labels[i]) == 0:
-                self.qtgui_freq_sink_x_0.set_line_label(i, "Data {0}".format(i))
-            else:
-                self.qtgui_freq_sink_x_0.set_line_label(i, labels[i])
-            self.qtgui_freq_sink_x_0.set_line_width(i, widths[i])
-            self.qtgui_freq_sink_x_0.set_line_color(i, colors[i])
-            self.qtgui_freq_sink_x_0.set_line_alpha(i, alphas[i])
-
-        self._qtgui_freq_sink_x_0_win = sip.wrapinstance(self.qtgui_freq_sink_x_0.qwidget(), Qt.QWidget)
-        self.top_layout.addWidget(self._qtgui_freq_sink_x_0_win)
-        self.network_udp_sink_0 = network.udp_sink(gr.sizeof_float, 1, '127.0.0.1', 5000, 0, 1024, True)
-        self.blocks_nlog10_ff_0 = blocks.nlog10_ff(10, 1, 0)
-        self.blocks_multiply_xx_0 = blocks.multiply_vcc(1)
-        self.blocks_moving_average_xx_0 = blocks.moving_average_ff(50000, 20, 4000, 1)
-        self.blocks_conjugate_cc_0 = blocks.conjugate_cc()
-        self.blocks_complex_to_real_0 = blocks.complex_to_real(1)
+        self.soapy_bladerf_source_1.set_sample_rate(0, samp_rate)
+        self.soapy_bladerf_source_1.set_bandwidth(0, bandwidth)
+        self.soapy_bladerf_source_1.set_frequency(0, centre_freq)
+        self.soapy_bladerf_source_1.set_frequency_correction(0, 0)
+        self.soapy_bladerf_source_1.set_gain(0, min(max(10.0, -1.0), 60.0))
+        self.blocks_nlog10_ff_1 = blocks.nlog10_ff(20, 1, 0)
+        self.blocks_complex_to_mag_squared_0 = blocks.complex_to_mag_squared(1)
+        self.blocks_abs_xx_0 = blocks.abs_ff(1)
 
 
         ##################################################
         # Connections
         ##################################################
-        self.connect((self.blocks_complex_to_real_0, 0), (self.blocks_moving_average_xx_0, 0))
-        self.connect((self.blocks_conjugate_cc_0, 0), (self.blocks_multiply_xx_0, 1))
-        self.connect((self.blocks_moving_average_xx_0, 0), (self.blocks_nlog10_ff_0, 0))
-        self.connect((self.blocks_multiply_xx_0, 0), (self.blocks_complex_to_real_0, 0))
-        self.connect((self.blocks_nlog10_ff_0, 0), (self.network_udp_sink_0, 0))
-        self.connect((self.blocks_nlog10_ff_0, 0), (self.qtgui_freq_sink_x_0, 0))
-        self.connect((self.blocks_nlog10_ff_0, 0), (self.qtgui_histogram_sink_x_0, 0))
-        self.connect((self.blocks_nlog10_ff_0, 0), (self.qtgui_number_sink_0, 0))
-        self.connect((self.soapy_bladerf_source_0, 0), (self.blocks_conjugate_cc_0, 0))
-        self.connect((self.soapy_bladerf_source_0, 0), (self.blocks_multiply_xx_0, 0))
+        self.connect((self.blocks_abs_xx_0, 0), (self.zeromq_pub_sink_0, 0))
+        self.connect((self.blocks_complex_to_mag_squared_0, 0), (self.blocks_nlog10_ff_1, 0))
+        self.connect((self.blocks_nlog10_ff_1, 0), (self.blocks_abs_xx_0, 0))
+        self.connect((self.soapy_bladerf_source_1, 0), (self.blocks_complex_to_mag_squared_0, 0))
 
 
     def closeEvent(self, event):
@@ -239,23 +111,21 @@ class TYP(gr.top_block, Qt.QWidget):
 
     def set_samp_rate(self, samp_rate):
         self.samp_rate = samp_rate
-        self.soapy_bladerf_source_0.set_sample_rate(0, self.samp_rate)
+        self.soapy_bladerf_source_1.set_sample_rate(0, self.samp_rate)
 
     def get_centre_freq(self):
         return self.centre_freq
 
     def set_centre_freq(self, centre_freq):
         self.centre_freq = centre_freq
-        self.qtgui_freq_sink_x_0.set_frequency_range(self.centre_freq, self.bandwidth)
-        self.soapy_bladerf_source_0.set_frequency(0, self.centre_freq)
+        self.soapy_bladerf_source_1.set_frequency(0, self.centre_freq)
 
     def get_bandwidth(self):
         return self.bandwidth
 
     def set_bandwidth(self, bandwidth):
         self.bandwidth = bandwidth
-        self.qtgui_freq_sink_x_0.set_frequency_range(self.centre_freq, self.bandwidth)
-        self.soapy_bladerf_source_0.set_bandwidth(0, self.bandwidth)
+        self.soapy_bladerf_source_1.set_bandwidth(0, self.bandwidth)
 
 
 
--- dataProcessor.py
+++ dataProcessor.py
@@ -1,22 +1,22 @@
-import os
-import sys
 import time
 import socket
-import random
-import threading
 import numpy as np
 import statistics as s
 import matplotlib.pyplot as plt
+import zmq
+
+def milli_timestamp():
+    return time.monotonic() * 1000
 
 def setimg(rgbval):
     image = plt.imshow([[rgbval]])
     return image
 
-def updateimg(rgbval, image):
+def updateimg(rgbval, image, timing):
     print(rgbval)
     image.set_data([[rgbval]])
     plt.draw()
-    plt.pause(1)
+    plt.pause(timing / 1000)
 
 def rgb(val):
     b = int(max(0, 255 * (1 - val)))
@@ -24,46 +24,35 @@ def rgb(val):
     g = 255 - b - r
     return r, g, b
 
-milli_timestamp = lambda: int(round(time.time() * 1000))
 def readData(conn, timing):
-    flag = False
-    tid = 0
+    t = milli_timestamp()
     first = True
-    image = 0
-
+    img = 0
     while True:
-        try:
-            if flag == False:
-                tid = milli_timestamp()
-                flag = True
-
-            ts = milli_timestamp()
-
-            data = conn.recv(1024)
-            if not data:
-                break
-
-            if (ts - tid) < timing and flag == True:
+        dt = milli_timestamp()
+        if (dt - t) > timing:
+            t, dt = milli_timestamp(), milli_timestamp()
+            if conn.poll() != 0:
+                msg = conn.recv()
+                print(len(msg))
+                data = np.frombuffer(msg, dtype=np.float32, count=-1)
+                print(s.mean(data))
                 if first:
-                    image = setimg(rgb(s.mean(data) / 100))
+                    img = setimg(rgb((100 - s.median(data)) / 100))
                     first = False
                 else:
-                    updateimg(rgb(s.median(data) / 100), image)
-            
-            else:
-                flag = False
-        except Exception as e:
-            print("Error occurred")
-            print(e)
-
-            break
-    return
+                    updateimg(rgb((100 - s.median(data)) / 100), img, timing)
+        else:
+            empty_sink = conn.recv()
 
 def runStream():
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    sock.bind(("127.0.0.1", 5000))
+    context = zmq.Context()
+    socket = context.socket(zmq.SUB)
+    socket.connect("tcp://127.0.0.1:5000")
+    socket.setsockopt(zmq.SUBSCRIBE, b'')
 
-    readData(sock, 1000)
+
+    readData(socket, 250)
     return
 
 def main():
